6ea94546ab60700c174b1eea76db81a15b0ef3a7
滕飞 <ArrayAdapter@yeah.net>
2016-11-11 16:15:29 +0800

    增加了接口文件 UrlUtils

---
+++ app/src/main/java/share/example/qiongyou/util/UrlUtils.java
@@ -0,0 +1,15 @@
+package share.example.qiongyou.util;
+
+/**
+ * 所有的接口文件都在这里
+ */
+public class UrlUtils {
+    /*首页接口*/
+    public static String HomePageUrl="http://open.qyer.com/qyer/home/home_feed?client_id=qyer_android&client_secret=9fcaae8aefc4f9ac4915&v=1&track_deviceid=285594210309348&track_app_version=7.0.5&track_app_channel=xiaomi&track_device_info=hwC8813Q&track_os=Android4.4.2&app_installtime=1478695259330&lat=-33.868844&lon=151.209175&page=1";
+    /*目的地接口*/
+    public static String DestinationUrl="http://open.qyer.com/qyer/discount/zk/discount_index?client_id=qyer_android&client_secret=9fcaae8aefc4f9ac4915&v=1&track_deviceid=285594210309348&track_app_version=7.0.5&track_app_channel=xiaomi&track_device_info=hwC8813Q&track_os=Android4.4.2&app_installtime=1478695259330&lat=-33.868844&lon=151.209175";
+    /*商城接口*/
+    public static String EmporiumUrl="http://open.qyer.com/qyer/discount/zk/discount_index?client_id=qyer_android&client_secret=9fcaae8aefc4f9ac4915&v=1&track_deviceid=285594210309348&track_app_version=7.0.5&track_app_channel=xiaomi&track_device_info=hwC8813Q&track_os=Android4.4.2&app_installtime=1478695259330&lat=-33.868844&lon=151.209175";
+    /*社区接口*/
+    public static String CommunityUrl="http://open.qyer.com/qyer/community/hotbbs/index?client_id=qyer_android&client_secret=9fcaae8aefc4f9ac4915&v=1&track_deviceid=285594210309348&track_app_version=7.0.5&track_app_channel=xiaomi&track_device_info=hwC8813Q&track_os=Android4.4.2&app_installtime=1478695259330&lat=-33.868844&lon=151.209175&page=1&count=10";
+}
